2e5cc8e1dbdfab7dcbea27c62659f8546a732084
Peter Marsh <p.marsh@mac.com>
2019-01-10 10:21:04 -0500

    Update urls.py
    deleted extra line

--- Djeddit/urls.py
+++ Djeddit/urls.py
@@ -42,7 +42,6 @@ urlpatterns = [
     path('subdelete/<int:subreddit>/', views.DeleteSubView.as_view()),
     path('r/<slug:subreddit>/delete/<int:post>/', views.delete_post_view),
     path('p/<int:post>/delete/', views.delete_individual_post_view),
-    path('subdelete/<int:subreddit>/', views.delete_sub_view),
     path('p/<int:post>/reply/<int:comment>/', views.reply_view),
     path('p/<int:post>/delete/<int:reply>/', views.delete_reply_view)
 ]
