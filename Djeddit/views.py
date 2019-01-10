e1a54a670d2c65a75327dbe20bc307f5d7204599
Bensch97 <ben.w.schacht@gmail.com>
2019-01-10 15:21:36 -0500

    500 not working

--- Djeddit/views.py
+++ Djeddit/views.py
@@ -22,16 +22,14 @@ from Djeddit.utils import handle_vote, get_user_votes
 from django.template import RequestContext
 
 
-def handler404(request, *args, **argv):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
+def handler404(request, exception, template_name="404.html"):
+    response = render_to_response("404.html")
     response.status_code = 404
     return response
 
 
-def handler500(request, *args, **argv):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
+def handler500(request, exception, template_name="500.html"):
+    response = render_to_response("500.html")
     response.status_code = 500
     return response
 
