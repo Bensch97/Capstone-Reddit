0130a4c7d8cae0a5cfea84033227575e4618e7af
Colin Bruin <colinjb23@gmail.com>
2019-01-11 10:45:54 -0500

    and were done

--- Djeddit/settings.py
+++ Djeddit/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3ku&m78a9($p==#%(*aq_+(l3#s*w)o2@5c=_mr#w!+_3*2iua'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
